0e2b9d243b941b9b28d697f63398000278485d6a
Ri-ft <20175877+Ri-ft@users.noreply.github.com>
2019-05-02 13:59:00 -0500

    Update models.py

--- models.py
+++ models.py
@@ -15,16 +15,20 @@ class Teacher(models.Model):
 
 class Section(models.Model):
     name = models.CharField(max_length=30)
+    #each section is taught by a teacher
     teacher = models.ForeignKey(Teacher, related_name = 'sections' on_delete=models.CASCADE)
+    #student can potentially be in many sections, and a section has many students
     students = models.ManyToManyField(Student, related_name = 'sections')
 
 class Test(models.Model):
     name = models.CharField(max_length=30)
+    #each test pertains to a student
     student = models.ForeignKey(Student, related_name = 'tests', on_delete=models.CASCADE)
     grade = models.DecimalField()
 
 class Project(models.Model):
     name = models.CharField(max_length=30)
+    #each project pertains to a student
     student = models.ForeignKey(Student, related_name = 'projects', on_delete=models.CASCADE)
     grade = models.DecimalField()
 
@@ -32,6 +36,7 @@ class Question(models.Model):
     question = models.TextField()
     answer = models.TextField()
     score = models.IntegerField()
+    #each question pertains to a test
     test = models.ForeignKey(Test, related_name = 'questions', on_delete=models.CASCADE)
 
 class Goal(models.Model):
@@ -39,6 +44,7 @@ class Goal(models.Model):
     completed = models.BooleanField()
     start_date = models.DateField()
     goal_date = models.DateField()
+    #each goal pertains to a student
     student = models.ForeignKey(Student, related_name = 'goals', on_delete=models.CASCADE)
 
     
